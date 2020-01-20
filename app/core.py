17228b86021bff5ccfc5294e7529d28c86b97532
okolodezev <kod.j@ya.ru>
2020-01-20 17:10:53 +0700

    дописал генератор так, чтобы он брал данные из after_table_edited

--- app/core.py
+++ app/core.py
@@ -2,15 +2,52 @@ from enum import Enum
 from jinja2 import Template
 
 
+def initColumns(columns, key, object):
+    row_type = type(object[key])
+    if row_type == str or row_type == int:
+        columns.append(
+            Column(title=key, type=ColumnType.cell, template_str="{{item." + key + "}}")
+        )
+
+    elif row_type == dict:
+        columns.append(
+            Column(
+                title=key,
+                type=ColumnType.dict,
+                template_str="{{key}}  ---  {{item[key]}}",
+            )
+        )
+
+    elif row_type == list:
+        first_array_row = type(object[key][0])
+
+        if first_array_row == str or row_type == int:
+            columns.append(
+                Column(title=key, type=ColumnType.array, template_str="{{subitem}}")
+            )
+        if first_array_row == dict:
+            for row_key in object[key][0].keys():
+                columns.append(
+                    Column(
+                        title=row_key,
+                        type=ColumnType.array,
+                        array_key=key,
+                        template_str="{{subitem." + row_key + "}}",
+                    )
+                )
+
+    return True
+
+
 class ColumnType(Enum):
     simple_cell = 0
     cell = 1
     array = 2
-    dict_keys = 3
+    dict = 3
 
 
 class Column(object):
-    def __init__(self, title, type, template_str, array_key=''):
+    def __init__(self, title, type, template_str, array_key=""):
         self.title = title
         self.array_key = array_key
         self.type = type
@@ -29,24 +66,27 @@ class Column(object):
             return Template(
                 """
 		<div class="list">
-			{%for subitem in item.""" + self.array_key +  """%}
-				<div class="cell">"""+
-					self.template_str + 
-				"""</div>
+			{%for subitem in item."""
+                + self.array_key
+                + """%}
+				<div class="cell">"""
+                + self.template_str
+                + """</div>
 			{% endfor%}
 		</div class="list">"""
             ).render(item=input)
 
-        
-        # if self.type == ColumnType.key_value_dict:
-        #     return Template(
-        #         """
-		# <div class="list">
-		# 	{%for subitem in item.keys()%}
-		# 		<div class="cell">"""+
-		# 			self.template_str + 
-		# 		"""</div>
-		# 	{% endfor%}
-		# </div class="list">""")
-        
+        if self.type == ColumnType.dict:
+            return Template(
+                """
+		<div class="list">
+			{%for key in item.keys()%}
+				<div class="cell">"""
+                + self.template_str
+                + """</div>
+			{% endfor%}
+		</div class="list">"""
+            ).render(item=input)
 
+    def set_descripton(self, desc):
+        self.desc = desc
--- app/index.py
+++ app/index.py
@@ -5,173 +5,46 @@ from enum import Enum
 import requests
 from jinja2 import Template
 from lxml import etree
-from lxml.cssselect import CSSSelector
-from lxml.etree import fromstring
 
-urls = {}
-urls["tore"] = "http://cirtec.ranepa.ru/cirtec/top/ref_bundles/"
-urls["taur"] = "http://cirtec.ranepa.ru/cirtec/top/ref_authors/"
-
-urls["raut"] = "http://cirtec.ranepa.ru/cirtec/pubs/ref_authors/"
-
-urls["frap"] = "http://cirtec.ranepa.ru/cirtec/frags/publications/"
-urls["foci"] = "http://cirtec.ranepa.ru/cirtec/frags/cocitauthors/"
-urls["cauc"] = "http://cirtec.ranepa.ru/cirtec/frags/cocitauthors/cocitauthors/"
-urls["rebu"] = "http://cirtec.ranepa.ru/cirtec/ref_bund4ngramm_tops/?topn=100"
-urls["aubu"] = "http://cirtec.ranepa.ru/cirtec/ref_auth4ngramm_tops/"
-urls["febu"] = "http://cirtec.ranepa.ru/cirtec/frags/ref_bundles/"
-# urls["refa"] = "http://cirtec.ranepa.ru/cirtec/frags/ref_authors/"
-urls["popu"] = "http://cirtec.ranepa.ru/cirtec/pos_neg/pubs/"
-urls["pobu"] = "http://cirtec.ranepa.ru/cirtec/pos_neg/ref_bundles/"
-urls["paut"] = "http://cirtec.ranepa.ru/cirtec/pos_neg/ref_authors/"
-urls["toco"] = "http://cirtec.ranepa.ru/cirtec/frags/topics/ngramms/"
-urls["taut"] = "http://cirtec.ranepa.ru/cirtec/frags/topics/cocitauthors/"
-urls["fato"] = "http://cirtec.ranepa.ru/cirtec/frags/ngramms/topics/"
-urls["nagu"] = "http://cirtec.ranepa.ru/cirtec/frags/ngramms/cocitauthors/"
-urls["foat"] = "http://cirtec.ranepa.ru/cirtec/frags/cocitauthors/topics/"
-urls["fano"] = "http://cirtec.ranepa.ru/cirtec/frags/cocitauthors/ngramms/"
-urls["frat"] = "http://cirtec.ranepa.ru/cirtec/frags/topics/"
-urls["legn"] = "http://cirtec.ranepa.ru/cirtec/frags/ngramms/?ltype=lemmas"
-urls["copu"] = "http://cirtec.ranepa.ru/cirtec/top/cocitauthors/publications/"
-urls["pung"] = "http://cirtec.ranepa.ru/cirtec/top/ngramms/publications/?ltype=lemmas"
-urls["topu"] = "http://cirtec.ranepa.ru/cirtec/top/topics/publications/"
-urls["tabu"] = "http://cirtec.ranepa.ru/cirtec/pos_neg/ref_bundles/"
-urls["tona"] = "http://cirtec.ranepa.ru/cirtec/pos_neg/ref_authors/"
-urls["tang"] = "http://cirtec.ranepa.ru/cirtec/pos_neg/ngramms/"
-urls["tato"] = "http://cirtec.ranepa.ru/cirtec/pos_neg/topics/"
-urls["taco"] = "http://cirtec.ranepa.ru/cirtec/pos_neg/cocitauthors/"
-# urls["tant"] = "http://cirtec.ranepa.ru/cirtec/frags/pos_neg/contexts/"
-
-
-class ColumnType(Enum):
-    simple_cell = 0
-    cell = 1
-    array = 2
-    dict = 3
-
-
-class Column(object):
-    def __init__(self, title, type, template_str, array_key=""):
-        self.title = title
-        self.array_key = array_key
-        self.type = type
-        self.template_str = template_str
-
-    def get_template(self, input):
-        # if (self.type == ColumnType.simple_cell):
-        # 	return Template('<div>{{item}}</div>'.format(self.template_str)).render(item=input)
-
-        if self.type == ColumnType.cell:
-            return Template("<div>{}</div>".format(self.template_str)).render(
-                item=input
-            )
-
-        if self.type == ColumnType.array:
-            return Template(
-                """
-		<div class="list">
-			{%for subitem in item."""
-                + self.array_key
-                + """%}
-				<div class="cell">"""
-                + self.template_str
-                + """</div>
-			{% endfor%}
-		</div class="list">"""
-            ).render(item=input)
-
-        if self.type == ColumnType.dict:
-            return Template(
-                """
-		<div class="list">
-			{%for key in item.keys()%}
-				<div class="cell">"""
-                + self.template_str
-                + """</div>
-			{% endfor%}
-		</div class="list">"""
-            ).render(item=input)
-
-
-def initColumns(columns, key, object):
-    row_type = type(first_row[key])
-    if row_type == str or row_type == int:
-        columns.append(
-            Column(title=key, type=ColumnType.cell, template_str="{{item." + key + "}}")
-        )
-
-    elif row_type == dict:
-        columns.append(
-            Column(
-                title=key,
-                type=ColumnType.dict,
-                template_str="{{key}}  ---  {{item[key]}}",
-            )
-        )
-
-    elif row_type == list:
-        # first_array_row = type(first_row[key][0])
-
-        if row_type == str or row_type == int:
-            columns.append(
-                Column(title=key, type=ColumnType.array, template_str="{{subitem}}")
-            )
-        if row_type == dict:
-            for row_key in first_row[key].keys():
-                columns.append(
-                    Column(
-                        title=key,
-                        type=ColumnType.array,
-                        template_str="{{subitem" + row_key + "}}",
-                    )
-                )
-
-    return True
-
+from app.core import initColumns
 
 parser = etree.XMLParser
 xml = {}
 
-with open("cec_new/after_table1.xml", mode="r", encoding="utf-8") as xml_raw:
-    xml = fromstring(xml_raw.read())
-    print(xml)
+doc = etree.parse("after_table_edited.xml")
+items = [*doc.findall("section/item"), *doc.findall("section/*/item")]
+# items = [*doc.findall("section/item")][:1]
+print(len(items))
 
-for url_key in urls:
-# url_key = list(urls.keys())[0]
+for item in items:
+    url_key = item.attrib["id"]
+    print("Начинаем обработку элемента с id = {url}".format(url=url_key))
 
-    url_xml_item = xml.cssselect("item#" + url_key)[0]
+    url = item.attrib["source"] # там лежит уже готовый url
+    # data = {}
+    data = requests.get(url).json()
+    print("Получен ответ с url {key} - {type}".format(key=url_key, type=type(data)))
 
-    title = url_xml_item.cssselect("title")[0]
-    url_xml_desc = url_xml_item.cssselect("desc")[0]
-    url_xml_legend = url_xml_item.cssselect("legend")[0]
-    url_xml_expl = url_xml_item.cssselect("expl")[0]
-
-
-    request = requests.get(urls[url_key])
-    print("{key} : {code}".format(key=url_key, code=request.status_code))
-
-
-    data = request.json()
+    title = item.find("title").findtext("ru")
+    desc = item.find("desc").find("ru")
+    expl = item.find("expl").find("ru")
 
     columns = []
     jinja2_args = {
         "data": data,
         "columns": columns,
-        "title": etree.tostring(title, encoding="unicode"),
-        "others": [
-            etree.tostring(url_xml_desc),
-            etree.tostring(url_xml_legend),
-            etree.tostring(url_xml_expl),
-        ],
+        "title": title,
+        "desc": etree.tostring(desc, encoding="unicode"),
+        "expl": etree.tostring(expl, encoding="unicode"),
     }
 
-    # по первой строчке создаём схему
+    # Инициализация колонок табилцы
     first_row = {}
     if type(data) == list:
         jinja2_args["data_type"] = 1
         first_row = data[0]
 
-    if type(data) == dict:
+    elif type(data) == dict:
         jinja2_args["data_type"] = 2
         jinja2_args["data_keys"] = list(data.keys())
         first_row = data[list(data.keys())[0]]
@@ -179,8 +52,23 @@ for url_key in urls:
     for key in first_row.keys():
         initColumns(columns=columns, key=key, object=first_row)
 
-    html = open("cec_new/template 4.html", encoding="utf-8").read()
+    legend = item.find("legend") # вставляем в каждую колонку данные о легенде
+    if (legend is not None):
+        for element in list(legend.find("ru").getiterator()):
+            if "title" not in element.attrib:
+                continue
+
+            title = element.attrib["title"]
+            cols = [col for col in columns if col.title == title]
+            if (len(cols) == 1):
+                cols[0].desc = element.text
+
+
+    html = open("app/template 4.html", encoding="utf-8").read()
     template = Template(html)
 
-    with open("cec_new/output/{}.html".format(url_key), "w", encoding="utf-8") as output:
+    with open("output/{}.html".format(url_key), "w", encoding="utf-8") as output:
         output.write(template.render(**jinja2_args))
+    with open("saved_json/{}.json".format(url_key), "w", encoding="utf-8") as output:
+        output.write(json.dumps(data, indent=4))
+        
